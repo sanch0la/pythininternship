b00d0044c2065f7db239f41a08a9acc77b860f19
jordan <kiserajordan33@gmail.com>
2025-11-27 00:41:29 +0300

    initial commit

---
+++ sum.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def main():
+    x = int(input("Key in a number X:"))
+    y = int(input("Key in a number Y:"))
+
+    print(x + y)
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
