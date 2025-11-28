67bdc999bd7535f96d1ead7498c57cc3fbb70f4f
jordan <kiserajordan33@gmail.com>
2025-11-28 08:49:31 +0300

    initial commit

---
+++ exercise_8.py
@@ -0,0 +1,12 @@
+import sys
+
+def main():
+   word =  input("Enter a word:")
+   second_word = input("Enter a second word:")
+
+   print(f"{word} {second_word}")
+
+   return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
