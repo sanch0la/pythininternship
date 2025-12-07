b0f78e7b7f9f0106c9bd11e4784fb9991bf6fdac
jordan <kiserajordan33@gmail.com>
2025-12-07 22:34:39 +0300

    initial commit

---
+++ exercise_16.py
@@ -0,0 +1,20 @@
+"""
+Create a variable x = None. Print it.
+Ask the user to then assign a value to x via input. Print the new value.
+"""
+
+import sys
+
+
+def main():
+    x = None
+    print(x)
+
+    x = int(input("Enter a number: "))
+    print(x)
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
