cd7682f4d7b79a6eab820b67da95e43fcedc4886
jordan <kiserajordan33@gmail.com>
2025-12-13 17:32:09 +0300

    initial commit

---
+++ exercise_17.py
@@ -0,0 +1,19 @@
+"""
+Call print() on a message and assign it to a variable:
+
+v = print("Hello")
+
+Print v and observe its value.
+"""
+
+
+import sys
+
+
+def main():
+    name  = print("My name is Jordan")
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
