edb7609dcfb078489117460334b1e2873f9c9455
jordan <kiserajordan33@gmail.com>
2025-12-24 23:55:13 +0300

    initial commit

--- prac.py
+++ prac.py
@@ -1,17 +1,11 @@
 import sys
+from operator import index, indexOf
+
 
 def main():
-    try:
-        number_a = int(input("Enter a number a: "))
-        number_b = int(input("Enter a number b: "))
-    except ValueError:
-        print("Invalid input: please enter whole numbers.")
-        return 1
-
-    print("a > b:", number_a > number_b)
-    print("a == b:", number_a == number_b)
-    print("a != b:", number_a != number_b)
-
+    for number in range(1, 100):
+        if number % 7 == 0 or number % 11 == 0:
+            print(number)
     return 0
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -2,20 +2,9 @@ import sys
 
 
 def main():
-    # Ask the user for five words
-    words = [
-        input("Enter word 1: "),
-        input("Enter word 2: "),
-        input("Enter word 3: "),
-        input("Enter word 4: "),
-        input("Enter word 5: ")
-    ]
-
-    # Construct a new list with words starting with 'a' or 'A'
-    a_words = [w for w in words if w[:1] == "a" or w[:1] == "A"]
-
-    print("Words starting with 'a' or 'A':", a_words)
-
+    x = int(input("x:"))
+    y = 2 * x ** 4 + 3
+    print(y)
     return 0
 
 
