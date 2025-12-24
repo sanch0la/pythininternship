92d28ee549aedaf957c88824a75a335243d34698
jordan <kiserajordan33@gmail.com>
2025-12-25 00:49:02 +0300

    initial commit

---
+++ exercise_39.py
@@ -0,0 +1,14 @@
+"""
+Print the result of data.values().
+"""
+import sys
+
+
+def main():
+    data = {"a": 1, "b": 2, "c": 3}
+    print(data.values())
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
