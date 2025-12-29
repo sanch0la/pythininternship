d5b92e1f0739d8c23bde6660be7e35c363ef48be
jordan <kiserajordan33@gmail.com>
2025-12-29 08:53:46 +0300

    initial commit

---
+++ exercise_50.py
@@ -0,0 +1,26 @@
+""""
+Ask the user for two integers a and b.
+
+If both are positive, print "Both positive".
+
+Otherwise, print "At least one is not positive".
+"""
+import sys
+
+
+def main():
+    try:
+        number_a = int(input("Enter a number a: "))
+        number_b = int(input("Enter a number b: "))
+        if number_a and number_b > 0:
+            print("Both positive")
+        else:
+            print("At least one is not positive")
+    except ValueError:
+        print("Please enter a number.")
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
