1113f45052e6da3792bd2235a0f95d901d179167
jordan <kiserajordan33@gmail.com>
2025-12-31 00:05:21 +0300

    initial commit

--- prac.py
+++ prac.py
@@ -2,20 +2,9 @@ import sys
 
 
 def main():
-    grades = {
-        "A": 80,
-        "B": 70,
-        "C": 60,
-        "D": 50
-    }
-
-    grade = input("Enter a grade letter (A, B, C, or D): ").upper()
-
-    if grade in grades:
-        print(f"The minimum score for grade {grade} is {grades[grade]}")
-    else:
-        print("Invalid grade letter.")
-
+    words = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    for word in range(len(words)):
+        print(f"{word} == {words[word]}")
     return 0
 
 
