c01450c3dcd468f82929f0223474ff4207a8ff67
jordan <kiserajordan33@gmail.com>
2025-12-31 12:34:24 +0300

    initial commit

---
+++ exercise_69.py
@@ -0,0 +1,23 @@
+""""
+Given:
+
+items = ["apple", "banana", "cherry"]
+
+Using the enumerate() function and an for-construct print:
+
+0: apple
+1: banana
+2: cherry
+"""
+import sys
+
+
+def main():
+    items = ["apple", "banana", "cherry"]
+    for index,item in enumerate(items):
+        print(f"{index}: {item}")
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
