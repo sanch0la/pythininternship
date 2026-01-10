73c42a6d61c01f71b8f8ffd53719b0d2b67e3001
jordan <kiserajordan33@gmail.com>
2026-01-10 11:53:34 +0300

    complete

--- test.py
+++ test.py
@@ -2,9 +2,8 @@ import sys
 
 
 def main():
-    school = ["Books","pens","pupils","Uniform", "tie"]
-    for i in range(len(school)):
-        print(f"{i} ==> {school[i]}")
+    x = int(input())
+    print(x)
 
 
 if __name__ == '__main__':
