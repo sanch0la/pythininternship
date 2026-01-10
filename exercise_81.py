032d96b244299026855f80effd783d59eee5c03c
jordan <kiserajordan33@gmail.com>
2026-01-10 10:18:21 +0300

    complete

--- exercise_81.py
+++ exercise_81.py
@@ -9,10 +9,13 @@ import sys
 def main():
     def trial_x():
         x = 10
-
+    print(x)
     trial_x()
+
+
     return 0
 
-# The output is blank. It just displays that the process finishes with an exit code of 0
+# We get a name error. This is because we are printing x (The variable) outside the function block hence it
+#it doesn't know whats happening outside this block
 if __name__ == '__main__':
     sys.exit(main())
