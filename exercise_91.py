4701c18c26797f87c745fdf4da5ccc0d30cda72c
jordan <kiserajordan33@gmail.com>
2026-01-10 12:28:25 +0300

    complete

---
+++ exercise_91.py
@@ -0,0 +1,19 @@
+""""
+Define grade_label(score) that returns "Pass" or "Fail".
+"""
+import sys
+
+
+def main():
+    def grade_label(score):
+        if score >= 40:
+            return "Pass"
+        else:
+            return "Fail"
+    grade_label(49)
+    print(grade_label(9))
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
