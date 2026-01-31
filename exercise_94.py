6a89d497d7f2b8d5b63f2948b11f946f557d4efa
jordan <kiserajordan33@gmail.com>
2026-01-31 12:06:14 +0300

    complete

--- exercise_94.py
+++ exercise_94.py
@@ -7,23 +7,20 @@ import sys
 
 
 def main():
-    def simple_interest(principal,rate, time):
-        return principal*(rate/100)*time
-    def total_amount(principal,interest):
-        return principal + simple_interest(principal,interest,time)
+    def simple_interest(principal, rate, time):
+        return principal * (rate / 100) * time
+
+    def total_amount(principal, interest):
+        return principal + simple_interest(principal, interest, time)
+
     principal = float(input("Enter the principal: "))
-    interest = float(input("Enter the interest: "))
+    interest = float(input("Enter the rate: "))
     time = float(input("Enter the time: "))
 
     simple_interest(principal, interest, time)
     print(f"The simple interest is {simple_interest(principal, interest, time)}")
     print(f"The total amount is {total_amount(principal, interest)}")
     total_amount(principal, interest)
-
-
-
-
-
 
     return 0
 
