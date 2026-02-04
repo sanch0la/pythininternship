7d55364fb1542f71c161328edffaecd8029771ff
jordan <kiserajordan33@gmail.com>
2026-02-04 17:26:22 +0300

    complete

---
+++ exercise_100.py
@@ -0,0 +1,18 @@
+""""
+Given any text file (you choose):
+
+a) Function to read and normalize text (remove end of line characters)
+b) Function to count words
+c) Function to identify top N words
+d) Print results neatly in a table
+"""
+import sys
+
+
+def main():
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
